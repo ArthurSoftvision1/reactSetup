070d4ee230327eaeab74de02c217016926087210
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-11-17 14:08:01 +0200

    Form validation

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+            <li>TEXT</li>
+            <li>TEXT</li>
+            <li>TEXT</li>
+            <li>TEXT</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
